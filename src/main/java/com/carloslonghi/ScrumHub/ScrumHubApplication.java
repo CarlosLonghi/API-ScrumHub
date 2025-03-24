70f339b9e8ab664577e082e859fe203f67f971cc
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-03-24 14:44:24 -0300

    Initial Commit

---
+++ src/main/java/com/carloslonghi/ScrumHub/ScrumHubApplication.java
@@ -0,0 +1,13 @@
+package com.carloslonghi.ScrumHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumHubApplication.class, args);
+	}
+
+}
