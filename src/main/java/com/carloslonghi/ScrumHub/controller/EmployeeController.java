0b423949a3beccf7fada4f8e466f35276890b48c
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-04-03 11:38:27 -0300

    feat: add `endpoints` to `employee` controller

--- src/main/java/com/carloslonghi/ScrumHub/controller/EmployeeController.java
+++ src/main/java/com/carloslonghi/ScrumHub/controller/EmployeeController.java
@@ -1,16 +1,33 @@
 package com.carloslonghi.ScrumHub.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/employee")
 public class EmployeeController {
 
-    @GetMapping("/api/v1/employee")
-    public String hello() {
-        return "Você acessou a rota de funcionários";
+    @GetMapping
+    public String getAll() {
+        return "Rota para listar todos os funcionários";
     }
 
+    @PostMapping
+    public String create() {
+        return "Rota para criação de um funcionário";
+    }
+
+    @GetMapping("/{id}")
+    public String getById() {
+        return "Rota para busca de funcionário por id";
+    }
+
+    @PutMapping("/{id}")
+    public String updateById() {
+        return "Rota para alteração de funcionário por id";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteById() {
+        return "Rota para deleção de funcionário por id";
+    }
 }
